1d7f70489f52940bae85a7c4db94862e484e36b2
Pierre VIGNAUD <pvignaud@PHILO.INTRA>
2018-06-12 09:58:01 +0200

    page lol et changement sur index

--- lol.php
+++ lol.php
@@ -2,11 +2,9 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <title>Document</title>
+    <title>LOL</title>
 </head>
 <body>
-<form action="POST">
-    <input type="submit" value="envoyer">
-</form>
+<h1>le plus LOL du monde</h1>
 </body>
 </html>
